bda119aed5b5c3b287b8a3931cbd95bcd428439e
github username <github email address>
2019-03-12 16:34:47 -0400

    import gql on LinksList.js, import dependencies, update return function.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,10 @@
-import React, { Component } from 'react';
-
-import '../styles/App.css';
+import React, { Component } from 'react'
+import LinkList from './LinkList'
 
 class App extends Component {
   render() {
-    return (
-      <div>
-
-      </div>
-    );
+    return <LinkList />
   }
 }
 
-export default App;
+export default App
